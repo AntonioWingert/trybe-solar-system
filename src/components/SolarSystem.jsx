b7193a1e91464a830adea3a6e29fa00cb913a9a5
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-10-21 15:33:43 -0300

    req 6 complet

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 export default class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="planetas" />
+        <ul>
+          {planets
+            .map((planet) => (
+              <PlanetCard
+                key={ planet.name }
+                planetName={ planet.name }
+                planetImage={ planet.image }
+              />))}
+        </ul>
       </div>
     );
   }
