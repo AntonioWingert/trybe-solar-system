82885fe60c86fa7610794cc1c1f9a8059354d4aa
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-10-21 14:55:01 -0300

    req 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
     return (
       <main>
         <Header />
+        <SolarSystem />
+        <Title />
         <p>Sistema Solar</p>
       </main>
     );
